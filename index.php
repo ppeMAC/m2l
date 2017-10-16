0cd55c1fc2ed6640bee404316825e96116ea2f79
Marvin <Marvin@172.17.1.166>
2017-10-16 14:35:18 +0200

    essai marvin

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ phpinfo();
 // as defined in config.inc.php using the variable $default_view
 // If $default_room is defined in config.inc.php then this will
 // be used to redirect to a particular room.
+//ESSAI
 
 require_once "defaultincludes.inc";
 require_once "mrbs_sql.inc";
